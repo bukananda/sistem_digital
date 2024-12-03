6e0f9d3d48bdf5b931da81d7d9f9fabd22b29a37
bukananda <bukansayawoi@gmail.com>
2024-12-03 19:56:34 +0700

    Keystream dibuat secara serial

--- stream_cipher.vhd
+++ stream_cipher.vhd
@@ -6,11 +6,11 @@ entity stream_cipher is
     generic(data_length: integer := 32);
     port (
         key: in unsigned((data_length*8)-1 downto 0);
-        counter: in unsigned(data_length-1 downto 0);
         nonce: in unsigned((data_length*3)-1 downto 0);
+        plain_text: in unsigned(7 downto 0);
         clk,start,rst: in std_logic;
-        en_out: out std_logic;
-        child_key: out unsigned((data_length*16)-1 downto 0)
+        cipher_text: out unsigned(7 downto 0);
+        en_out: out std_logic
     );
 end entity;
 
@@ -18,9 +18,10 @@ architecture behavioral of stream_cipher is
 
     type array_mat is array (0 to 15) of unsigned(data_length-1 downto 0);
 
-    type state is (idle, state1, finished);
+    type state is (idle, state1, finished, xor_process);
     signal curr_state,next_state: state;
     signal curr_count,next_count,curr_bit,next_bit: integer := 0;
+    signal curr_ctr,next_ctr: unsigned(data_length-1 downto 0) := x"00000001";
     signal my_array2: array_mat;
 
     procedure quarter_round(
@@ -51,15 +52,17 @@ begin
         elsif rising_edge(clk) then
             curr_state <= next_state;
             curr_count <= next_count;
+            curr_ctr <= next_ctr;
         end if;
     end process change_state;
 
-    chacha20 : process(start,curr_state,curr_count,key,counter,nonce)
+    chacha20 : process(start,curr_state,curr_count,key,curr_ctr,next_ctr,nonce)
         variable key_temp: unsigned((data_length*8)-1 downto 0);
         variable counter_temp: unsigned(data_length-1 downto 0);
         variable nonce_temp: unsigned((data_length*3)-1 downto 0);
         variable my_array,my_array_idle: array_mat;
         variable child_key_temp: unsigned((data_length*16)-1 downto 0);
+        variable plain_text_temp: unsigned(7 downto 0);
         constant cons_1: unsigned(data_length-1 downto 0) := x"61707865";
         constant cons_2: unsigned(data_length-1 downto 0) := x"3320646e";
         constant cons_3: unsigned(data_length-1 downto 0) := x"79622d32";
@@ -69,7 +72,7 @@ begin
             if start ='1' then
                 my_array := my_array2;
                 key_temp := unsigned(key);
-                counter_temp := unsigned(counter);
+                counter_temp := unsigned(curr_ctr);
                 nonce_temp := unsigned(nonce);
 
                 -- Baris 0
@@ -128,10 +131,21 @@ begin
                 my_array(i) := (my_array2(i) + my_array_idle(i));
                 child_key_temp(((data_length*16)-(32*i)-1) downto ((data_length*15)-(32*i))) := my_array(i)(7 downto 0) & my_array(i)(15 downto 8) & my_array(i)(23 downto 16) & my_array(i)(31 downto 24);
             end loop;
-                
             next_count <= 0;
-            child_key <= child_key_temp;
-            next_state <= finished;
+            next_state <= xor_process;
+
+        elsif curr_state = xor_process then
+            if curr_count > 63 then
+                next_ctr <= curr_ctr + 1;
+                next_count <= 0;
+                en_out <= '0';
+                next_state <= idle;
+            else
+                next_count <= curr_count + 1;
+                cipher_text <= child_key_temp(((data_length*16)-1)-(8*(curr_count)) downto ((data_length*16)-8*(curr_count+1)));
+                en_out <= '1';
+                next_state <= xor_process;
+            end if;
         end if;
     end process chacha20;
 end architecture behavioral;
